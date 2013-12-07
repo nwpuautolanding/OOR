0c9de718c7814963868987e38cb191480ff2a980
fox <fox@abr-Satellite-S855D.(none)>
2013-12-07 19:12:44 +0000

    adding linking script

---
+++ SourceCode/ROS/createLinks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <path to ros src workspace>"
+fi
+
+currentDir="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for node in $(ls -d */)
+do
+#	echo "$currentDir/$node to $1/"
+	cleanNode="${node%?}"
+	sourceDir="$currentDir/$cleanNode"
+	destDir="$1/$cleanNode"
+	echo "ln -s $sourceDir $destDir"
+	ln -s $sourceDir $destDir
+done
