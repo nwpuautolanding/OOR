97f87aa1b52bf4673cbe215fa2ca40b22f0def24
fox <fox@abr-Satellite-S855D.(none)>
2013-12-08 18:10:42 +0000

    small change in main of plane segmentation

--- SourceCode/ROS/lobos_plane_segmentation/src/main.cpp
+++ SourceCode/ROS/lobos_plane_segmentation/src/main.cpp
@@ -7,6 +7,7 @@
 #include <lobos_cloud_pubsub/CloudPublisher.hpp>
 #include <lobos_cloud_pubsub/RangeImageSubscriber.hpp>
 #include <string>
+#include <iostream>
 
 #include "rangeImage_utils.hpp"
 
@@ -18,19 +19,19 @@ int main (int argc, char **argv) {
     CloudPublisher<pcl::PointXYZRGB>  my_cloudPublisher (nh, std::string("planes_segmented"));
     RangeImageSubscriber my_rangeImageSubscriber (nh, std::string("/camera/depth/image_rect"), std::string("/camera/depth/camera_info"));
 
-    ros::AsyncSpinner spinner(4); // Use 4 threads
+    ros::AsyncSpinner spinner(1); 
     spinner.start();
-
+    ros::Rate r(1)
     while (ros::ok()) {
 
         if (my_rangeImageSubscriber.getIsThereNewData()) {
-		pcl::RangeImage ri = my_rangeImageSubscriber.getCurrentRangeImage();
+            pcl::RangeImage ri = my_rangeImageSubscriber.getCurrentRangeImage();
 
 	        computePlaneExtraction(ri.makeShared());	
-
-
+            std::cout << "Cloud computed " << std::endl;
 
         }
+        r.sleep();
     }
 
 
