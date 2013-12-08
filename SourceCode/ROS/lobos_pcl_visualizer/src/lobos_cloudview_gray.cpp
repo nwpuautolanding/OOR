e15a9923b0042bd4e1a3e212688889fe8e96aa86
abraham <abr@abr-Satellite-S855D.(none)>
2013-12-08 19:36:01 +0000

    some changes

--- SourceCode/ROS/lobos_pcl_visualizer/src/lobos_cloudview_gray.cpp
+++ SourceCode/ROS/lobos_pcl_visualizer/src/lobos_cloudview_gray.cpp
@@ -5,26 +5,24 @@
 #include <ros/ros.h>
 
 // PCL
-//~#include <pcl/ros/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-
 #include <pcl/visualization/pcl_visualizer.h>
 #include <sensor_msgs/PointCloud2.h>
-
 #include <pcl/conversions.h>
- #include <sensor_msgs/PointCloud2.h>
- #include <pcl_conversions/pcl_conversions.h>
-
- #include <iostream>
- #include <boost/thread/thread.hpp>
- #include <lobos_cloud_pubsub/cloudSubscriber.hpp>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <iostream>
+#include <boost/thread/thread.hpp>
+#include <lobos_cloud_pubsub/CloudSubscriber.hpp>
 #include <pcl/console/parse.h>
-//variables for visualizarion custom
+
+// Default variables for visualizarion custom
 int r=0;
 int g=0;
 int b=255;
-int psize=0.5;
+int psize=0.1;
+//defintion of the type fo point cloud to be used in the visualizer
 typedef pcl::PointXYZ PointT;
 
 boost::shared_ptr<pcl::visualization::PCLVisualizer> createVis ()
@@ -40,62 +38,42 @@ boost::shared_ptr<pcl::visualization::PCLVisualizer> createVis ()
 }
 
  
-
-/** 
- */
 int main(int argc, char **argv) {
   using namespace ros;
+  //Ros intializaton, arg[1] is the name that will be givne to the node, this is useful when running several instances of the node, remember that twio nodes with the same name can't run
   ros::init(argc,argv,argv[1]);
   ros::NodeHandle nh;
-  bool update = false;
-  bool rgb=false;
+  
   if (argc<3)
   {
-	  printf("Incorrect number of argument ,usage: [std:: name of node to create][name of topic to subscribe]");
+	  printf("Incorrect number of arguments ,usage: [std::string name of node to create][std::string::name of topic of the XYZ PC  to subscribe][optional  r g b values for visualization of the point cloud]");
 	  return 0;
   }
-  
-  
-      
-	  pcl::PointCloud<PointT>::Ptr ptCloud (new pcl::PointCloud<PointT>);
-	  pcl::visualization::PointCloudColorHandlerCustom<PointT> color_p_cloud(ptCloud, r, g, b); 
-	
-  
-       CloudSubscriber<PointT>  cs (nh, argv[2]);
-   
- 
-   
-   ros::AsyncSpinner spinner(1); // Use 1 threads
-   
-    
-   spinner.start();
-   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
-   viewer = createVis();
+  if (argc>3)
+  {
+	  r= atoi(argv[3]);
+	  g= atoi(argv[4]);
+	  b= atoi(argv[5]);
+  }
+  pcl::PointCloud<PointT>::Ptr ptCloud (new pcl::PointCloud<PointT>);
+  pcl::visualization::PointCloudColorHandlerCustom<PointT> color_p_cloud(ptCloud, r, g, b); 
+  CloudSubscriber<PointT>  cs (nh, argv[2]);
+  ros::AsyncSpinner spinner(1); // Use 1 threads
+  spinner.start();
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+  viewer = createVis();
    while (!viewer->wasStopped ())
    {
             viewer->spinOnce (1);
-           
-            
-            if(cs.getIsThereNewData())
+        if(cs.getIsThereNewData())
             {
 				*ptCloud = cs.getCurrentPointclout();
-				
                 if(!viewer->updatePointCloud(ptCloud,color_p_cloud, "sample cloud"))
                   viewer->addPointCloud(ptCloud, color_p_cloud, "sample cloud");
                 
                 viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, psize, "sample cloud");
-                
-            }
-            
-
-     } 
-  
-  
-  
-    printf("despues de ros spin");	
+			}
+    } 
     spinner.stop();
-  
-  
-   
 	return 0;
 }
--- SourceCode/ROS/lobos_pcl_visualizer/src/lobos_cloudview_rgb.cpp
+++ SourceCode/ROS/lobos_pcl_visualizer/src/lobos_cloudview_rgb.cpp
@@ -5,28 +5,20 @@
 #include <ros/ros.h>
 
 // PCL
-//~#include <pcl/ros/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <sensor_msgs/PointCloud2.h>
-
 #include <pcl/conversions.h>
- #include <sensor_msgs/PointCloud2.h>
- #include <pcl_conversions/pcl_conversions.h>
-
- #include <iostream>
- #include <boost/thread/thread.hpp>
- #include <lobos_cloud_pubsub/cloudSubscriber.hpp>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <iostream>
+#include <boost/thread/thread.hpp>
+#include <lobos_cloud_pubsub/CloudSubscriber.hpp>
 #include <pcl/console/parse.h>
-//variables for visualizarion custom
-int r=0;
-int g=0;
-int b=255;
-int psize=0.5;
+//defintion of the type fo point cloud to be used in the visualizer
 typedef pcl::PointXYZRGBA PointT;
-
+int psize = 0.5;
 boost::shared_ptr<pcl::visualization::PCLVisualizer> createVis ()
 {
   // --------------------------------------------
@@ -39,10 +31,6 @@ boost::shared_ptr<pcl::visualization::PCLVisualizer> createVis ()
   
 }
 
- 
-
-/** 
- */
 int main(int argc, char **argv) {
   using namespace ros;
   ros::init(argc,argv,argv[1]);
@@ -51,29 +39,18 @@ int main(int argc, char **argv) {
   bool rgb=false;
   if (argc<3)
   {
-	  printf("Incorrect number of argument ,usage: [std:: name of node to create][name of topic to subscribe]");
+	  printf("Incorrect number of arguments ,usage: [std::string name of node to create][std::string::name of topic of the XYZRGBA PC to subscribe]");
 	  return 0;
   }
   
-  
-      
-	  pcl::PointCloud<PointT>::Ptr ptCloud (new pcl::PointCloud<PointT>);
-	  pcl::visualization::PointCloudColorHandlerRGBField<PointT> color_p_cloud(ptCloud);
-  
-  	//~pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_p_cloud(ptCloud, r, g, b); 
-	
-  
-       CloudSubscriber<PointT>  cs (nh, argv[2]);
-   
- 
-   
-   ros::AsyncSpinner spinner(1); // Use 1 threads
-   
-    
-   spinner.start();
-   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
-   viewer = createVis();
-   while (!viewer->wasStopped ())
+  pcl::PointCloud<PointT>::Ptr ptCloud (new pcl::PointCloud<PointT>);
+  pcl::visualization::PointCloudColorHandlerRGBField<PointT> color_p_cloud(ptCloud);
+  CloudSubscriber<PointT>  cs (nh, argv[2]);
+  ros::AsyncSpinner spinner(1); // Use 1 threads
+  spinner.start();
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+  viewer = createVis();
+  while (!viewer->wasStopped ())
    {
             viewer->spinOnce (1);
             // Get lock on the boolean update and check if cloud was updated
@@ -86,18 +63,8 @@ int main(int argc, char **argv) {
                   viewer->addPointCloud(ptCloud, color_p_cloud, "sample cloud");
                 
                 viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, psize, "sample cloud");
-                
             }
-            
-
-     } 
-  
-  
-  
-    printf("despues de ros spin");	
+        } 
     spinner.stop();
-   //~ros::waitForShutdown();
-  
-   
-	return 0;
+   	return 0;
 }
