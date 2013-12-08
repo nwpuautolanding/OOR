278eedb2eb8fd15280167f9edbf8f380f018bfde
fox <fox@abr-Satellite-S855D.(none)>
2013-12-08 02:20:07 +0000

    primera cosa feta

--- SourceCode/ROS/lobos_cloud_pubsub/include/lobos_cloud_pubsub/cloudSubscriber.hpp
+++ SourceCode/ROS/lobos_cloud_pubsub/include/lobos_cloud_pubsub/cloudSubscriber.hpp
@@ -3,7 +3,10 @@
 #define CLOUD_SUBSCRIBER
 
 #include <string>
-#include <pcl/ros/conversions.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 
 template <typename T>
@@ -17,45 +20,62 @@ private:
     pcl::PointCloud<T> localPointCloud;
     // Ros subscriber
     ros::Subscriber sub;
+    bool isThereNewData;
 
     //Private methods
     void callback (const sensor_msgs::PointCloud2ConstPtr& input);
 
 public:
-    CloudSubscriber (ros::NodeHandle nh);
+    CloudSubscriber (ros::NodeHandle nh,std::string topicName);
     ~CloudSubscriber();
     
     /**
      * Getters
      */
     pcl::PointCloud<T> getCurrentPointclout ();
+    bool getIsThereNewData();
 
-}
+};
 
 template <typename T>
 CloudSubscriber<T>::CloudSubscriber (ros::NodeHandle nh, std::string topicName) {
     
-    sub = nh.subscribe (topicName, 1, &CloudSubscriber::callback, this);
+    isThereNewData = false;
+    sub = nh.subscribe (topicName, 1, &CloudSubscriber<T>::callback, this);
     
 }
 
 template <typename T>
-CloudSubscriber<t>::~CloudSubscriber() {
+CloudSubscriber<T>::~CloudSubscriber() {
 
 }
 
 template <typename T>
-void CloudSubscriber::callback (const sensor_msgs::PointCloud2ConstPtr& input) {
+void CloudSubscriber<T>::callback (const sensor_msgs::PointCloud2ConstPtr& input) {
 
-   localPointCloud = pcl::fromROSMsg (input);
+   //localPointCloud = pcl::fromROSMsg (input);
+   pcl::fromROSMsg<T>(*input, localPointCloud);
+   isThereNewData = true;
+
+}
+
+/**
+* Getters
+*/
+
+template <typename T>
+pcl::PointCloud<T> CloudSubscriber<T>::getCurrentPointclout() {
+
+    isThereNewData = false;
+    return localPointCloud;
 
 }
 
 template <typename T>
-pcl::PointCloud<T> CloudSubscriber<T>::getCurrentPointcloud() {
+bool CloudSubscriber<T>::getIsThereNewData () {
 
-    retunr localPointCloud;
+   return isThereNewData;
+
 }
-
 
 #endif
--- SourceCode/ROS/lobos_plane_segmentation/src/main.cpp
+++ SourceCode/ROS/lobos_plane_segmentation/src/main.cpp
@@ -1,5 +1,17 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 
-int main () {
+#include <lobos_cloud_pubsub/cloudSubscriber.hpp>
+#include <string>
 
+int main (int argc, char **argv) {
+    ros::init (argc, argv, "my_pcl_tutorial");
+    ros::NodeHandle nh;
+    CloudSubscriber<pcl::PointXYZ>  cs (nh, std::string("hola"));
+
+    ros::spin();
     return 0;
 }
